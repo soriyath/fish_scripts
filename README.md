54404c92795c81ac2f4c17f8920df3904dde4ee4
soriyath <soriyath@straessle.io>
2019-10-20 17:28:52 +0200

    Make check_ssl_certificate.fish more intelligent

--- README.md
+++ README.md
@@ -8,11 +8,15 @@ Checks if an SSL certificate is still valid.
 
 ### installation
 
-Copy file `check_ssl_certificate.fish` into `~/.config/fish/functions/`
+Make a symbolic link in Fish's functions directory:
+`ln -s <your repo>/check_ssl_certificate.fish ~/.config/fish/functions/`
 
 ### usage
 
-run `check_ssl_certificate hostname:port`
+run `check_ssl_certificate hostname port`
+e.g. `check_ssl_certificate www.google.com 443`
 
 this will yield `$status 0` if certificate is not expired<br />
 or `$status 1` if there is an error
+or `$status 2` if you forgot to give the hostname or port you want to check...
+
--- check_ssl_certificate.fish
+++ check_ssl_certificate.fish
@@ -1,12 +1,29 @@
 #!/usr/local/bin/fish
 # see: https://unix.stackexchange.com/questions/234970/script-to-check-if-ssl-certificate-is-valid#answer-234975
+# return codes:
+#  - 0: success;
+#  - 1: SSL certificate for given hostname and port are expired or will expire within the next month;
+#  - 2: there are missing parameters.
 
 function check_ssl_certificate
-  if true | openssl s_client -connect $argv 2>/dev/null | openssl x509 -noout -checkend 0;
-    echo "$argv SSL Certificate is not expired"
+  set HOST $argv[1]
+  set PORT $argv[2]
+  set ONE_MONTH 2592000
+  # set ONE_YEAR 31104000 # for debugging purpose
+  # set TEN_YEARS 311040000 # for debugging purpose
+  # set ONE_HUNDRED_YEARS 3110400000 # doesn't work but is fun
+
+  if test -z "$HOST" ; or test -z "$PORT"
+    echo -e "Hostname or a port are missing.\nUsage: check_ssl_certificate www.google.com 443"
+    return 2
+  end
+
+  if true | openssl s_client -connect $HOST:$PORT -servername $HOST </dev/null 2>/dev/null | \
+    openssl x509 -noout -checkend $ONE_MONTH;
+    echo "$HOST:$PORT SSL Certificate is not expired within the next month"
     return 0
   else
-    echo "$argv SSL Certificate is either absent or expired"
+    echo "$HOST:$PORT SSL Certificate is either absent or expired within the next month"
     return 1
   end
 end
